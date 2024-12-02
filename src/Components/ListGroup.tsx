21b72930bfcbc014aa9c427c48af91fb231b9c7b
Cauê Alves <cauealvesb4@gmail.com>
2024-12-02 20:33:49 -0300

    Rederizando uma lista com o map

--- src/Components/ListGroup.tsx
+++ src/Components/ListGroup.tsx
@@ -1,18 +1,22 @@
-
-
 function ListGroup() {
+    const items = [
+        'Brazil',
+        'EUA',
+        'México',
+        'Canadá',
+        'Argentina'
+    ]
+
     return (
-    <div>
+    <>
         <h1>Olá</h1>
         <ul className="list-group">
-            <li className="list-group-item">An item</li>
-            <li className="list-group-item">A second item</li>
-            <li className="list-group-item">A third item</li>
-            <li className="list-group-item">A fourth item</li>
-            <li className="list-group-item">And a fifth one</li>
-            </ul>
-    </div>
-    );
+            {items.map((item) => (
+                <li key={item}>{item}</li>
+            ))}
+        </ul>
+    </> 
+    ); 
 }
 
 export default ListGroup
